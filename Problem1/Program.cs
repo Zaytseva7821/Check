c43957c1fe33cd5b449607d2606bc69fea736917
Anastasia <zaytseva7821@ya.ru>
2022-09-01 00:13:41 +0400

    problem 1

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -7,12 +7,17 @@ Console.WriteLine("Введите число 1");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2");
 int b = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("max = ");
 if (a < b)
 {
+    System.Console.Write("max = ");
     System.Console.WriteLine(b);
+    System.Console.Write("min = ");
+    System.Console.WriteLine(a);
 }
 else
 {
+    System.Console.Write("max = ");
     System.Console.WriteLine(a);
+    System.Console.Write("min = ");
+    System.Console.WriteLine(b);
 }
